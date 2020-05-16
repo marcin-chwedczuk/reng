44d14adf5454a7697ea67ea3c40caf836b2738cc
Marcin Chwedczuk <marcin@sumologic.com>
2020-05-16 07:51:02 +0200

    Renaming and cleanup in continuations

--- test/pl/marcinchwedczuk/continuations/Continuations1Test.java
+++ test/pl/marcinchwedczuk/continuations/Continuations1Test.java
@@ -5,20 +5,19 @@ import org.junit.Test;
 
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
-import java.util.function.Function;
 
 public class Continuations1Test {
     private interface Cont0<R> {
-        Ramp apply(R result);
+        Thunk apply(R result);
     }
 
-    private interface Ramp {
-        Ramp run();
+    private interface Thunk {
+        Thunk run();
     }
 
-    private static void exec(Ramp ramp) {
-        while (ramp != null) {
-            ramp = ramp.run();
+    private static void trampoline(Thunk thunk) {
+        while (thunk != null) {
+            thunk = thunk.run();
         }
     }
 
@@ -29,39 +28,40 @@ public class Continuations1Test {
         };
     }
 
-    private static Ramp add(int a, int b, Cont0<Integer> cont) {
-        int ab = a + b;
-        return () -> cont.apply(ab);
+    private static Thunk add(int a, int b, Cont0<Integer> cont) {
+        int sum = a + b;
+        return () -> cont.apply(sum);
     }
 
-    private static Ramp add(int a, int b, int c, Cont0<Integer> cont) {
-        return add(a, b, ab ->
-                add(ab, c, cont));
+    private static Thunk add(int a, int b, int c, Cont0<Integer> cont) {
+        return add(a, b, sum ->
+                add(sum, c, cont));
     }
 
-    private static Ramp multiply(int a, int b, Cont0<Integer> cont) {
-        int ab = a * b;
-        return () -> cont.apply(ab);
+    private static Thunk multiply(int a, int b, Cont0<Integer> cont) {
+        int product = a * b;
+        return () -> cont.apply(product);
     }
 
-    private static Ramp eq(int a, int b, Cont0<Boolean> cont) {
-        boolean aEqB = (a == b);
-        return () -> cont.apply(aEqB);
+    private static Thunk eq(int a, int b, Cont0<Boolean> cont) {
+        boolean result = (a == b);
+        return () -> cont.apply(result);
     }
 
-    private static Ramp lt(int a, int b, Cont0<Boolean> cont) {
-        return () -> cont.apply(a < b);
+    private static Thunk lt(int a, int b, Cont0<Boolean> cont) {
+        boolean result  = (a < b);
+        return () -> cont.apply(result);
     }
 
-    private static Ramp iff(boolean expr,
-                            Cont0<Boolean> trueBranch,
-                            Cont0<Boolean> falseBranch) {
+    private static Thunk iff(boolean expr,
+                             Cont0<Boolean> trueBranch,
+                             Cont0<Boolean> falseBranch) {
         return (expr)
                 ? () -> trueBranch.apply(true)
                 : () -> falseBranch.apply(false);
     }
 
-    private static Ramp factorial(int n, Cont0<Integer> cont) {
+    private static Thunk factorial(int n, Cont0<Integer> cont) {
         return eq(n, 0, isNZero ->
                 iff(isNZero,
                         trueArg -> cont.apply(1),
@@ -72,11 +72,11 @@ public class Continuations1Test {
 
     @Test public void ramping_works() {
         AtomicInteger res = new AtomicInteger(-1);
-        exec(factorial(4, endCall(res::set)));
+        trampoline(factorial(4, endCall(res::set)));
 
         Assert.assertEquals(24, res.get());
 
         // No stack overflow
-        exec(factorial(400000, endCall(res::set)));
+        trampoline(factorial(400000, endCall(res::set)));
     }
 }
